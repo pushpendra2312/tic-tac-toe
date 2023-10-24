ab99984d78a508c2e081d2d78852e7136857211e
pushpendra2312 <sg38482@gmail.com>
2023-10-24 14:43:17 +0530

    feat: create basic html structure

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,25 @@
+import "./Grid.css"
+
+const Grid = () => {
+  return (
+    <div className="mainContainer">
+      <div className="rowContainer">
+        <div className="cells"></div>
+        <div className="cells"></div>
+        <div className="cells"></div>
+      </div>
+      <div className="rowContainer">
+        <div className="cells"></div>
+        <div className="cells"></div>
+        <div className="cells"></div>
+      </div>
+      <div className="rowContainer">
+        <div className="cells"></div>
+        <div className="cells"></div>
+        <div className="cells"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Grid
---
+++ src/components/Grid/index.jsx
@@ -0,0 +1,2 @@
+import comp from "./Grid"
+export default comp
